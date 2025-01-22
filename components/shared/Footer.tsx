b127b9c23163f425fdac42737f658d7721c8431c
neelrudra2 <neel.rudra.mailbox@gmail.com>
2025-01-22 16:32:32 +0530

    updates

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="border-t">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
-        <Link href="/" className="w-36 flex items-center">
+        <Link href="/">
           <Image
             src="/assets/images/eventique.png"
             alt="logo main"
@@ -14,7 +14,7 @@ const Footer = () => {
           />
         </Link>
 
-        <p>© 2025 Eventique. All Rights reserved.</p>
+        <p>© 2025 Eventique. All Rights Reserved.</p>
       </div>
     </footer>
   );
